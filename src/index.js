5eabd24cc339d152acb99b03fe6ca906b8ec2fd9
AnaRativa09 <anarativa09@gmail.com>
2020-10-21 16:20:11 -0500

    Starting Home

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Start from './components/Start';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Start />
   </React.StrictMode>,
   document.getElementById('root')
 );
