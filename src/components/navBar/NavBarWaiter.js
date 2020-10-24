4482dbeb75ff526e1b0ddb73c9a5b6884b939832
AnaRativa09 <anarativa09@gmail.com>
2020-10-24 16:17:18 -0500

    Fix indent

--- src/components/navBar/NavBarWaiter.js
+++ src/components/navBar/NavBarWaiter.js
@@ -4,16 +4,15 @@ import home from '../../assets/svg/Home.svg';
 import '../../styles/NavBarWaiter.css';
 //import { Link } from 'react-router-dom';
 
-export default function NavBarWaiter () {
-return (
+export default function NavBarWaiter() {
+  return (
     <header className='Header-nav-waiter'>
         <ul className='Nav-waiter'>
-            <li className='Nav-waiter-item'><img src={home} className='Nav-home-icon' alt='Icon Home'/></li>
-            <li className='Nav-waiter-item'>Nuevo Pedido</li>
-            <li className='Nav-waiter-item'>Estado de pedido</li>
+          <li className='Nav-waiter-item'><img src={home} className='Nav-home-icon' alt='Icon Home'/></li>
+          <li className='Nav-waiter-item'>Nuevo Pedido</li>
+          <li className='Nav-waiter-item'>Estado de pedido</li>
         </ul>
         <img src={logo} className='Nav-waiter-logo' alt='Burguer Queeen Logo'/>
-
     </header>
-    )
+  )
 }
