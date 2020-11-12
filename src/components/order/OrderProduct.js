92d9b393b0622aabeb081c47c3827da35dd7fad8
marialejandraip <mariaibanez07@gmail.com>
2020-11-12 15:59:41 -0500

    changes

--- src/components/order/OrderProduct.js
+++ src/components/order/OrderProduct.js
@@ -11,18 +11,26 @@ export default function OrderProduct({ order, setOrder }) {
       <div className='Order-product' key={index}>    
         <p>{product}</p>
         <div className='Order-quantity'>
-          <button className='Btn-product' aria-label='Restar producto'onClick={()=>{ quantity - 1 === 0 ? setOrder(order.filter((_,idx) => idx !== index )): setQuantity(quantity-1)}}>
+          <button className='Btn-product' 
+          aria-label='Restar producto'
+          onClick={()=>{ quantity - 1 === 0 ? setOrder(order.filter((_,idx) => idx !== index )): setQuantity(quantity-1)}}>
             <i className='fas fa-minus'></i>
           </button>
           <p className='Order-number'>{quantity}</p>
-          <button className='Btn-product' aria-label='Sumar producto'onClick={()=>{setQuantity(quantity+1)}}>
+          <button className='Btn-product' 
+          aria-label='Sumar producto'
+          onClick={()=>{setQuantity(quantity+1)}}>
             <i className='fas fa-plus'></i>
           </button>
         </div>
         <div className='Order-price'>
+          
           <p className='Order-number'>{'price'}</p>
-          <button className='Btn-product' aria-label='Eliminar producto' >
-            <i className='fas fa-trash'onClick ={ () => setOrder(order.filter((_,idx) => idx !== index ))} ></i>
+
+          <button className='Btn-product' 
+          aria-label='Eliminar producto' 
+          onClick ={ () => setOrder(order.filter((_,idx) => idx !== index ))} >
+            <i className='fas fa-trash'></i>
           </button>
         </div>
       </div>
